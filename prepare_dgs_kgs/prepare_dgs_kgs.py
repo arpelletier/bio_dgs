bc901482d6af71048a1aac7300126a333e8cbc64
Alexander Russell Pelletier <arpelletier@scai3.cs.ucla.edu>
2023-03-29 14:24:07 -0700

    Finished work with prepare_dgs_kgs for debugging. Moving on to modeling

--- prepare_dgs_kgs/prepare_dgs_kgs.py
+++ prepare_dgs_kgs/prepare_dgs_kgs.py
@@ -172,55 +172,58 @@ def parse_input_list(file_name):
 	return name_to_files['ontologies'], name_to_files['bridges'], name_to_files['instances']
 
 
-def check_bridges(kg1_df, kg2_df, alignf_df, print_freq=False):
+def check_bridges(kg1_df, kg2_df, alignf_df, print_freq=False, out_file_name="readout.txt"):
 	'''
 	Bridge edges are assumed to be: 
 		'h' from kg1 (instances)
 		't' from kg2 (ontology)
 		'r' is not used
 	'''
-	kg1f_nodes = set(kg1_df['h']).union(set(kg1_df['t']))
-	kg2f_nodes = set(kg2_df['h']).union(set(kg2_df['t']))
-	
-	freq_bool_vec = {}
-	for h,t in zip(alignf_df['h'],alignf_df['t']):
+	with open(out_file_name,"w") as out_file:
+		kg1f_nodes = set(kg1_df['h']).union(set(kg1_df['t']))
+		kg2f_nodes = set(kg2_df['h']).union(set(kg2_df['t']))
 		
+		freq_bool_vec = {}
+		for h,t in zip(alignf_df['h'],alignf_df['t']):
+			
+			if print_freq:
+				bool_vec = (h in kg1f_nodes, h in kg2f_nodes, t in kg1f_nodes, t in kg2f_nodes)
+				if bool_vec not in freq_bool_vec:
+					freq_bool_vec[bool_vec]=0
+				freq_bool_vec[bool_vec]+=1
+			h_in_kg1f = (h in kg1f_nodes, h in kg2f_nodes)
+			t_in_kg2f = (t in kg1f_nodes, t in kg2f_nodes)
+			if not h_in_kg1f[0] or not t_in_kg2f[1]:
+				if not print_freq:
+					out_file.write(" ".join(str(s) for s in (h,h_in_kg1f,t,t_in_kg2f))+"\n")
+					#print(h,h_in_kg1f,t,t_in_kg2f)
+	
 		if print_freq:
-			bool_vec = (h in kg1f_nodes, h in kg2f_nodes, t in kg1f_nodes, t in kg2f_nodes)
-			if bool_vec not in freq_bool_vec:
-				freq_bool_vec[bool_vec]=0
-			freq_bool_vec[bool_vec]+=1
-		h_in_kg1f = (h in kg1f_nodes, h in kg2f_nodes)
-		t_in_kg2f = (t in kg1f_nodes, t in kg2f_nodes)
-		if not h_in_kg1f[0] or not t_in_kg2f[1]:
-			if not print_freq:
-				print(h,h_in_kg1f,t,t_in_kg2f)
-
-	if print_freq:
-		renaming_vector = {(True,False,False,True):"Correct edges",
-						(False,True,True,False):"Need to flip",
-						(False,False,False,True):"Dangling Instance (head)",
-						(True,False,False,False):"Dangling Instance (tail)",
-						(False,False,True,True):"Head missing, Tail found in onto and inst",
-						(True,True,False,False):"Tail missing, Head found in onto and inst",
-						(False,True,False,False):"Dangling instance (tail) and needs to flip",
-						(False,False,True,False):"Dangling instance (head) and needs to flip",
-						(True,True,False,False):"Head in both, Tail in neither",
-						(False,False,True,True):"Tail in both, Head in neither",
-						(True,True,True,True):"Head and Tail found in both",
-						(False,False,False,False):"Head and Tail found in none"}
-		for bv, name in renaming_vector.items():
-			if bv in freq_bool_vec:
-				freq = freq_bool_vec[bv]
-				if freq > 0:
-					print(name, freq)
+			renaming_vector = {(True,False,False,True):"Correct edges",
+							(False,True,True,False):"Need to flip",
+							(False,False,False,True):"Dangling Instance (head)",
+							(True,False,False,False):"Dangling Instance (tail)",
+							(False,False,True,True):"Head missing, Tail found in onto and inst",
+							(True,True,False,False):"Tail missing, Head found in onto and inst",
+							(False,True,False,False):"Dangling instance (tail) and needs to flip",
+							(False,False,True,False):"Dangling instance (head) and needs to flip",
+							(True,True,False,False):"Head in both, Tail in neither",
+							(False,False,True,True):"Tail in both, Head in neither",
+							(True,True,True,True):"Head and Tail found in both",
+							(False,False,False,False):"Head and Tail found in none"}
+			for bv, name in renaming_vector.items():
+				if bv in freq_bool_vec:
+					freq = freq_bool_vec[bv]
+					if freq > 0:
+						#print(name, freq)
+						out_file.write(" ".join(str(s) for s in (name,freq))+"\n")
 
 #input_directory = '/home/arpelletier/workspace/dgs/2023-03-08_github_dgs/biomedkg_edges_03-08-2023'
 #output_directory = '/home/arpelletier/workspace/dgs/2023-03-08_github_dgs/dgs_input'
 #input_lists = '/home/arpelletier/workspace/dgs/2023-03-08_github_dgs/bio_dgs/input_lists/all_kg_edges.txt'
-input_directory = '/home/arpelletier/workspace/dgs/2023-03-08_github_dgs/biomedkg_edges_03-22-2023'
+input_directory = '/home/arpelletier/workspace/dgs/2023-03-08_github_dgs/biomedkg_edges_03-23-2023'
 output_directory = '/home/arpelletier/workspace/dgs/2023-03-08_github_dgs/dgs_input'
-input_lists = '/home/arpelletier/workspace/dgs/2023-03-08_github_dgs/bio_dgs/input_lists/2023-03-22_all_kg_edges.txt'
+input_lists = '/home/arpelletier/workspace/dgs/2023-03-08_github_dgs/bio_dgs/input_lists/2023-03-23_all_kg_edges.txt'
 
 output_file_names = ['kg2f_ontologies.txt','alignf_bridges.txt','kg1f_instances.txt']
 output_file_names = [os.path.join(output_directory,f) for f in output_file_names]
@@ -228,39 +231,48 @@ ontologies, bridges, instances = parse_input_list(input_lists)
 
 include_headers=False
 include_weights=False
+output_readouts=True
 
 ontologies_edges_nodes = read_and_merge_files(input_directory, ontologies, rename_ontology=True)
 bridges_edges_nodes = read_and_merge_files(input_directory, bridges)
 instances_edges_nodes = read_and_merge_files(input_directory, instances)
 
-check_bridges(instances_edges_nodes[0],
+if output_readouts:
+	# frequency readout
+	check_bridges(instances_edges_nodes[0],
 				ontologies_edges_nodes[0],
-				bridges_edges_nodes[0], print_freq=True)
+				bridges_edges_nodes[0], 
+				print_freq=True, out_file_name = "a2023-03-29_freq_readout.txt")
+	# bridge readout
+	check_bridges(instances_edges_nodes[0],
+				ontologies_edges_nodes[0],
+				bridges_edges_nodes[0],
+				print_freq=False, out_file_name = "a2023-03-29_bridge_readout.txt")
 
-#summary_out = []
-#for out_file_name, edges_nodes in zip(output_file_names,[ontologies_edges_nodes,bridges_edges_nodes,instances_edges_nodes]):
-#	edges_df, nodes_df = edges_nodes
-#	
-#	# write edges file
-#	if include_weights:
-#		out_edges_df = edges_df
-#	else:
-#		out_edges_df = edges_df[['h','r','t']]
-#	out_edges_df.to_csv(out_file_name, index=False, sep="\t", header=include_headers)
-#	print(out_file_name, edges_df.shape)
-#
-#	# write edges file for grape
-#	grape_edges_name = out_file_name[:-4]+"_grape.txt"
-#	edges_df.to_csv(grape_edges_name, index=False, sep="\t")	
-#	# write nodes file for grape
-#	nodes_outfile_name = out_file_name.replace(".txt","")+"_nodes.txt"
-#	nodes_df.to_csv(nodes_outfile_name,index=False,sep="\t")
-#	print(nodes_outfile_name,nodes_df.shape)	
-#	
-#	# train-test split
-#	split_sizes = train_test_split(grape_edges_name,nodes_outfile_name)
-#	summary_out += [(out_file_name,split_sizes)]
-#
-#print("### Summary ###")
-#for out_file_name, split_sizes in summary_out:
-#	print(out_file_name,split_sizes)
+summary_out = []
+for out_file_name, edges_nodes in zip(output_file_names,[ontologies_edges_nodes,bridges_edges_nodes,instances_edges_nodes]):
+	edges_df, nodes_df = edges_nodes
+	
+	# write edges file
+	if include_weights:
+		out_edges_df = edges_df
+	else:
+		out_edges_df = edges_df[['h','r','t']]
+	out_edges_df.to_csv(out_file_name, index=False, sep="\t", header=include_headers)
+	print(out_file_name, edges_df.shape)
+
+	# write edges file for grape
+	grape_edges_name = out_file_name[:-4]+"_grape.txt"
+	edges_df.to_csv(grape_edges_name, index=False, sep="\t")	
+	# write nodes file for grape
+	nodes_outfile_name = out_file_name.replace(".txt","")+"_nodes.txt"
+	nodes_df.to_csv(nodes_outfile_name,index=False,sep="\t")
+	print(nodes_outfile_name,nodes_df.shape)	
+	
+	# train-test split
+	split_sizes = train_test_split(grape_edges_name,nodes_outfile_name)
+	summary_out += [(out_file_name,split_sizes)]
+
+print("### Summary ###")
+for out_file_name, split_sizes in summary_out:
+	print(out_file_name,split_sizes)
